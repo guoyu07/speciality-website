56e3e502e593ac90d494a00c8985e90326cae8ec
czp <czp3009@gmail.com>
2017-02-12 18:49:20 +0800

    init

---
+++ src/main/java/com/hiczp/web/speciality/Application.java
@@ -0,0 +1,15 @@
+package com.hiczp.web.speciality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by czp on 17-2-7.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
