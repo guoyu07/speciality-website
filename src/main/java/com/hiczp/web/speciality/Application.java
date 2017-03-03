48c7a488040d83559f18b505363805f8383b3416
czp <czp3009@gmail.com>
2017-03-03 15:05:44 +0800

    添加 spring-boot-admin

--- src/main/java/com/hiczp/web/speciality/Application.java
+++ src/main/java/com/hiczp/web/speciality/Application.java
@@ -1,5 +1,6 @@
 package com.hiczp.web.speciality;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by czp on 17-2-7.
  */
 @SpringBootApplication
+@EnableAdminServer
 public class Application {
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/hiczp/web/speciality/controller/admin/AdminController.java
+++ src/main/java/com/hiczp/web/speciality/controller/admin/AdminController.java
@@ -1,5 +1,8 @@
 package com.hiczp.web.speciality.controller.admin;
 
+import org.apache.catalina.util.ServerInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin")
 public class AdminController {
     @GetMapping("")
-    public ModelAndView index(ModelAndView modelAndView) {
+    public ModelAndView index(ModelAndView modelAndView, @Value("${project.version}") String projectVersion, @Value("${maven.build.timestamp}") String mavenBuildTimestamp) {
         modelAndView.setViewName("/admin/index");
-        return modelAndView;
+        return modelAndView.addObject("projectVersion", projectVersion)
+                .addObject("mavenBuildTimestamp", mavenBuildTimestamp)
+                .addObject("springBootVersion", SpringBootVersion.getVersion())
+                .addObject("ServerInfo", ServerInfo.class)
+                .addObject("System", System.class);
     }
 }
