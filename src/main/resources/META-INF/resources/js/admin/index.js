58897fd1639ccd28305b631db9cf0e2d1180928c
czp <czp3009@gmail.com>
2017-03-07 17:20:21 +0800

    在 /admin 增加 ip 来源查询功能

---
+++ src/main/resources/META-INF/resources/js/admin/index.js
@@ -0,0 +1,13 @@
+jQuery("td[data-ip]").each(function () {
+    var td = jQuery(this);
+    jQuery.ajax({
+        url: "http://ip-api.com/json/" + td.attr("data-ip"),
+        success: function (data) {
+            if (data.status != "success") {
+                td.text(data.message);
+            } else {
+                td.text(data.country + " " + data.regionName + " " + data.city);
+            }
+        }
+    });
+});
