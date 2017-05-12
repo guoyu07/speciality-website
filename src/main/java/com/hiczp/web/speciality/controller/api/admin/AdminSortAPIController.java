98117b4af4ff049634f50feb1e56213ff0531a63
czp <czp3009@gmail.com>
2017-05-13 02:17:58 +0800

    删除分类时, 将该分类的子分类的父分类改为该分类的父分类

--- src/main/java/com/hiczp/web/speciality/controller/api/admin/AdminSortAPIController.java
+++ src/main/java/com/hiczp/web/speciality/controller/api/admin/AdminSortAPIController.java
@@ -1,8 +1,10 @@
 package com.hiczp.web.speciality.controller.api.admin;
 
 import com.hiczp.web.speciality.entity.ArticleEntity;
+import com.hiczp.web.speciality.entity.SortEntity;
 import com.hiczp.web.speciality.repository.ArticleRepository;
 import com.hiczp.web.speciality.repository.SortRepository;
+import com.hiczp.web.speciality.service.SortService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,22 +20,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/sort")
 public class AdminSortAPIController {
+    private SortService sortService;
     private SortRepository sortRepository;
     private ArticleRepository articleRepository;
 
-    public AdminSortAPIController(SortRepository sortRepository, ArticleRepository articleRepository) {
+    public AdminSortAPIController(SortService sortService, SortRepository sortRepository, ArticleRepository articleRepository) {
+        this.sortService = sortService;
         this.sortRepository = sortRepository;
         this.articleRepository = articleRepository;
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteById(@PathVariable Integer id) {
+        SortEntity sortEntity = sortRepository.findOne(id);
         sortRepository.delete(id);
 
         //将该分类下的文章的分类都改为 0
         List<ArticleEntity> articleEntities = articleRepository.findBySort(id);
-        articleEntities.forEach(articleEntity -> articleEntity.setSort(0));
+        articleEntities.parallelStream().forEach(articleEntity -> articleEntity.setSort(0));
         articleRepository.save(articleEntities);
+
+        //将该分类下的分类的父分类改为该分类的父分类
+        List<SortEntity> sortEntities = sortService.getChildSorts(id);
+        sortEntities.parallelStream().forEach(childSortEntity -> childSortEntity.setParent(sortEntity.getParent()));
+        sortRepository.save(sortEntities);
 
         return new ResponseEntity(HttpStatus.OK);
     }
